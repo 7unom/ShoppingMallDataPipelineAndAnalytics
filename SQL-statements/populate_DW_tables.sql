ab98e1cb4dd1c58a4ae3acf35fa9d950dbce37ee
Joel Funom Joshua <135712369+7unom@users.noreply.github.com>
2023-10-17 10:28:10 +0100

    Update populate_DW_tables.sql

--- SQL-statements/populate_DW_tables.sql
+++ SQL-statements/populate_DW_tables.sql
@@ -23,9 +23,9 @@ VALUES
     (6, 'Microwave Oven', '1000W Countertop Microwave', 79.99, 39.99, 102),
     (7, 'Refrigerator', '25 cu. ft. French Door Fridge', 1499.99, 749.99, 102),
     (8, 'T-Shirt', 'Cotton T-shirt', 14.99, 7.49, 103),
-    (9, 'Dress', "Women's Evening Dress", 89.99, 44.99, 103),
+    (9, 'Dress', 'Womens Evening Dress', 89.99, 44.99, 103),
     (10, 'Polo shirts', 'Short-sleeved shirts with a collar and a few buttons at the neck', 19.99, 9.99, 103),
-    (11, 'Leather Jacket', "Men's Genuine Leather Jacket", 149.99, 74.99, 103),
+    (11, 'Leather Jacket', 'Men"s Genuine Leather Jacket', 149.99, 74.99, 103),
     (12, 'Running Shoes', 'Unisex running shoes', 79.99, 39.99, 104),
     (13, 'Dress shoes', 'Shoes designed for formal occasions', 99.99, 49.99, 104),
     (14, 'Casual shoes', 'Shoes designed for everyday wear', 39.99, 19.99, 104),
@@ -35,10 +35,9 @@ VALUES
     (18, 'Skirts', 'Dresses without sleeves', 39.99, 19.99, 103),
     (19, 'Jeans', 'Denim pants', 39.99, 19.99, 103),
     (20, 'Shorts', 'Short pants', 29.99, 14.99, 103),
-    (21, 'Sunglasses', "Eyewear that protects the eyes from the sun's harmful UV rays", 19.99, 9.99, 105),
+    (21, 'Sunglasses', 'Eyewear that protects the eyes from the suns harmful UV rays', 19.99, 9.99, 105),
     (22, 'Jewelry', 'Decorative items worn on the body, such as necklaces, earrings, and bracelets', 109.99, 54.99, 105),
     (23, 'Button-down shirt', 'Long-sleeved shirts with buttons down the front', 24.99, 12.49, 103),
     (24, 'Headphones', 'Noise-canceling headphones', 129.99, 64.99, 101)
     ;
-
 -- End of SQL Script
